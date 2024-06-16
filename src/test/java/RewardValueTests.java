d26173e17456e8d5e381ffe29da0133aec09c4c1
DHANUSH <93424539+Dhanush0109@users.noreply.github.com>
2024-06-16 14:43:04 +0530

    Add files via upload

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100.0;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035;
+        assertEquals(expectedMiles, rewardValue.convertCashToMiles(cashValue), 0.001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 10000.0;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.convertMilesToCash(milesValue), 0.001);
     }
 }
